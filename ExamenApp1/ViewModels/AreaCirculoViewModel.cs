0ccc8f411799c56ca1c26cd2070b7a71f63276b2
MoiPad92 <moipad85@gmail.com>
2024-03-20 20:44:47 -0600

    Archivos ViewModels

---
+++ ExamenApp1/ViewModels/AreaCirculoViewModel.cs
@@ -0,0 +1,72 @@
+﻿
+using ExamenApp1.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ExamenApp1.ViewModels
+{
+    public partial class AreaCirculoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double radio;
+
+        [ObservableProperty]
+        private double pi;
+
+        [ObservableProperty]
+        private double resultado;
+
+
+        [RelayCommand]
+
+        public void CalcularCirculo()
+        {
+            try
+            {
+                if (Validar())
+                {
+                    ExamenApp examenApp = new ExamenApp();
+                    examenApp.Radio = radio;
+                    examenApp.Pi = pi;
+
+                    Resultado = Math.PI * Math.Pow(Radio, 2);
+                }
+            
+
+            }catch (Exception ex)
+            {
+                Alerta("Error", $"Excepción{ex.Message}");
+            }
+
+        
+        }
+
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Radio = 0;
+            Pi = 0;
+            Resultado = 0;
+        }
+
+        private bool Validar()
+        {
+            if(Radio == 0)
+            {
+                Alerta("Avertencia!", "Los campos no pueden ir vacios, ingrese el radio");
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+       
+        public void Alerta(string tipo, string mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(tipo, mensaje, "Aceptar") );
+        }
+    }
+}
---
+++ ExamenApp1/ViewModels/AreaRectanguloViewModel.cs
@@ -0,0 +1,76 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExamenApp1.Models;
+
+namespace ExamenApp1.ViewModels
+{
+    public partial class AreaRectanguloViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double _base;
+
+        [ObservableProperty]
+        private double _altura;
+
+        [ObservableProperty]
+        private double _resultado;
+
+        [RelayCommand]
+        public void CalcularRectangulo()
+        {
+            try
+            {
+                if (Validar())
+                {
+                    ExamenApp examenApp = new ExamenApp();
+                    examenApp.Base = _base;
+                    examenApp.Altura = _altura;
+
+                    Resultado = Base * Altura;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Alerta("Error", $"Excepción{ex.Message}");
+            }
+
+        }
+
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Base = 0;
+            Altura = 0;
+            Resultado = 0;
+        }
+
+        private bool Validar()
+        {
+            if (Base == 0)
+            {
+                Alerta("Avertencia!", "Los campos no pueden ir vacios, la base");
+
+                return false;
+            }
+            else if (Altura == 0)
+            {
+                Alerta("Avertencia!", "Los campos no pueden ir vacios, ingrese la altura");
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public void Alerta(string tipo, string mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(tipo, mensaje, "Aceptar"));
+        }
+    }
+}
