363d55c153ac18b4c18b4a008dd0890afbb556aa
MoiPad92 <moipad85@gmail.com>
2024-03-20 20:44:29 -0600

    Archivo Models

---
+++ ExamenApp1/Models/ExamenApp.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ExamenApp1.Models
+{
+    internal class ExamenApp
+    {
+        public double Radio { get; set; }
+
+        public double Pi { get; set;} 
+        public double Base { get; set; }
+        public double Altura { get; set; }
+    }
+}
